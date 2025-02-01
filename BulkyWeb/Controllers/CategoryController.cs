a18d5e8c3517f746f1b56427cb8167565a039949
svijibe@gmail.com <kvlk5403>
2025-01-31 21:24:14 -0500

    Partial View & toastr for notifcation styling

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -33,6 +33,7 @@ namespace BulkyWeb.Controllers
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category created successfully"; 
                 return RedirectToAction("Index");
             }
             return View();
@@ -60,6 +61,7 @@ namespace BulkyWeb.Controllers
             {
                 _db.Categories.Update(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category updated successfully";
                 return RedirectToAction("Index");
             }
             return View();
@@ -85,6 +87,7 @@ namespace BulkyWeb.Controllers
                 return NotFound();
             _db.Categories.Remove(obj);
             _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
             return RedirectToAction("Index");            
         }
        }
