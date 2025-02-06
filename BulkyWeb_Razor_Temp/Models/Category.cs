5bc53990583d8a33eb0b62d9c4c52651c814f59a
svijibe@gmail.com <kvlk5403>
2025-02-05 21:09:24 -0500

    Added Razor Project

--- BulkyWeb_Razor_Temp/Models/Category.cs
+++ BulkyWeb_Razor_Temp/Models/Category.cs
@@ -1,7 +1,7 @@
-﻿using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
 
-namespace BulkyWeb.Models
+namespace BulkyWeb_Razor_Temp.Models
 {
     public class Category
 
@@ -14,7 +14,7 @@ namespace BulkyWeb.Models
         public string Name { get; set; }
 
         [DisplayName("Display Order")]
-        [Range(1,100,ErrorMessage ="Display Order must be between 1-100")]
+        [Range(1, 100, ErrorMessage = "Display Order must be between 1-100")]
         public int DisplayOrder { get; set; }
     }
 }
---
+++ BulkyWeb_Razor_Temp/Pages/Categories/Create.cshtml.cs
@@ -0,0 +1,27 @@
+using BulkyWeb_Razor_Temp.Data;
+using BulkyWeb_Razor_Temp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BulkyWeb_Razor_Temp.Pages.Categories
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        [BindProperty]
+        public Category Category { get; set; }
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public void OnGet()
+        {
+        }
+        public IActionResult OnPost()
+        {
+            _db.Categories.Add(Category);
+            _db.SaveChanges();
+            return RedirectToPage("Index");
+        }
+    }
+}
